640e42165807e30058cf99cefb8807d6d3814090
Shandeep Singh <shandeepsingh96@gmail.com>
2019-11-13 11:25:09 -0500

    fixed message

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -9,7 +9,7 @@ const OptionModal = (props) => (
         closeTimeoutMS={200}
         className="modal"
     >
-        <h3 className="modal__title">Selected Option</h3>
+        <h3 className="modal__title">I pick...</h3>
         {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
         
         <button
